d1845ca5a323e899c731064c88488dc3dfae2f05
ncloyd <ncloyd33@gmail.com>
2018-04-02 17:02:23 -0500

    removed Homepage container, for now

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 //import React, { Component } from 'react';
 import React from 'react';
 import './App.css';
-import HomePage from './containers/Home';
+// import HomePage from './containers/Home';
+import Jumbo from './components/Jumbo';
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import Footer from './components/Footer';
@@ -9,7 +10,7 @@ import Footer from './components/Footer';
 const App = () => (
   <div className="App">
   	<div>
-	  	<HomePage />
+	  	<Jumbo />
 	  	<About />
 	  	<Portfolio />
 		<Footer />
--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 const Jumbo = () => (
-  <div>	
 		<div className="card bg-dark text-white jumbo">
 				  <img className="card-img jumbo" src="http://lorempixel.com/400/200" alt="img" />
 					  <div className="card-img-overlay">
 					    <h5 className="card-title">blah</h5>
 					  </div>
 		</div>
-  </div>
 );
 
 export default Jumbo;
