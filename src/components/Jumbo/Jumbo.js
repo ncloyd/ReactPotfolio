ad7622cba259cfd1cec846d1abbe4ac81f9764c5
ncloyd <ncloyd33@gmail.com>
2018-04-29 20:15:40 -0500

    parallax added to jumbo component

--- src/components/Jumbo/Jumbo.js
+++ src/components/Jumbo/Jumbo.js
@@ -1,12 +1,21 @@
 import React from 'react';
 
 const Jumbo = () => (
-		<div className="bg-dark card text-white jumbo parallax-container index-banner">
-				<div className="parallax">
-				  <img className="card-img jumbo" src='/beach.jpg' alt="img" />
-				</div> 
+
+		<div className="parallax-container index-banner">
+		    <div className="section no-pad-bot">
+		      	<div className="container">
+		      		<h1 className="header center teal-text text-lighten-2">Title
+		      		</h1>
+		        	<div className="row center">
+		          		<h5 className="header col s12 light">A modern responsive front-end framework based on Material Design</h5>
+		        	</div>
+				</div>
+		    </div>
+		    <div className="parallax"><img src="/beach.jpg" alt="Unsplashed background img 1" />
+		    </div>
 		</div>
+
 );
 
 export default Jumbo;
-
