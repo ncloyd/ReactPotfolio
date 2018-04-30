adbd7591e2aaaace25b53a06a0256de1e1346a93
ncloyd <ncloyd33@gmail.com>
2018-04-29 22:22:33 -0500

    added media query for portfolio and cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -10,25 +10,25 @@ class Card extends Component {
 		  		<div className="card-body sq">
 		  			<a href="https://afternoon-sea-84029.herokuapp.com/index" target="blank">
 		  				<img className="card-img sample" src="/burger3.jpg" alt="pic"/>
-		  	    		<p className="card-img-overlay card-text"></p>
+		  	    		<p className="card-text"></p>
 		  			</a>
 		  		</div>
 		  		<div className="card-body sq">
 		  			<a href="https://github.com/ncloyd/restaurant" target="blank">
 		  				<img className="card-img sample" src="/restfront.jpg" alt="pic"/>
-		  	    		<p className="card-img-overlay card-text"></p>
+		  	    		<p className="card-text"></p>
 		  			</a>
 		  		</div>
 		  		<div className="card-body sq">
 		  			<a href="https://github.com/ncloyd/weddingplanner" target="blank">
 		  				<img className="card-img sample" src='/wedding.jpg' alt="pic"/>
-		  	    		<p className="card-img-overlay card-text"></p>
+		  	    		<p className="card-text"></p>
 		  			</a>
 		  		</div>
 		  		<div className="card-body sq">
 		  			<a href="https://secret-refuge-42418.herokuapp.com/" target="blank">
 		  				<img className="card-img sample" src='/map.jpg' alt="pic"/>
-		  	    		<p className="card-img-overlay card-text"></p>
+		  	    		<p className="card-text"></p>
 		  			</a>
 		  		</div>
 		  	</div>
